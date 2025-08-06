951fa9dd8eccc631c35c7fc232e869b30b77c56b
janjaw007 <chayanit.kitm@gmail.com>
2025-08-06 07:51:01 +0700

    finish setup dbcontext and User entity and Users Table Context

--- InventoryAPI/Models/Entities/User.cs
+++ InventoryAPI/Models/Entities/User.cs
@@ -5,5 +5,6 @@
         public Guid Id { get; set; }
         public required string Username { get; set; }
         public required string Password { get; set; }
+        public required string Level { get; set; }
     }
 }
