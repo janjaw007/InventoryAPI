cfede3f143fccd18d1e2ad6cdd4f89a03bc88d72
janjaw007 <chayanit.kitm@gmail.com>
2025-08-06 07:48:18 +0700

    Add project files.

---
+++ InventoryAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using InventoryAPI.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace InventoryAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
